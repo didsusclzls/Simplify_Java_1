df3ac161705a15b4016cccf2f849812d5934eb90
didsusclzls <81861578+didsusclzls@users.noreply.github.com>
2021-04-03 23:38:13 +0900

    Add files via upload

---
+++ How_to_use_data.java
@@ -0,0 +1,21 @@
+
+public class How_to_use_data {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*		
+		자바는 출력할 데이터를 만들고 메모리에 넣는다.
+		int i = 10;	double i2= 20;
+		
+		i<double    (메모리 크기에서 double이 더 크다.)
+					(개발자는 데이터 크기를 알맞게 무조건 조절을 해야한다.)
+	
+			(이건 데이터크기의 상관 관계다.)
+double(8byte)=long>float(4byte)=int>short(2byte)=char>byte(1byte)=boolean
+
+		
+		
+	*/	
+	}
+
+}
